c2b025d5c9c425364e287ed53576fe9298b4b049
Sergeev, Pavel [External] <Pavel.Sergeev@externals.adidas.com>
2019-03-04 16:47:34 +0300

    homework 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-ReactJS Global Mentoring Program, homework
+ReactJS Global Mentoring Program, homework 1
+
+Module #1: Core concepts
+
+steps:
+1. checkout hw1 branch 
+2. yarn install
+3. npm run serve.dev
+4. open http://localhost:8080/
--- src/components/Component.component.tsx
+++ src/components/Component.component.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export class ComponentComponent extends React.Component<{}> {
+export class ComponentComponent extends React.Component {
     public render() {
         return <h2>Hello from Component component</h2>;
     }
