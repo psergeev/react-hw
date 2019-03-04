1c758449b96c2790a8c43545694fa68243d1d5ed
Sergeev, Pavel [External] <Pavel.Sergeev@externals.adidas.com>
2019-03-04 16:47:34 +0300

    homework 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import {ComponentComponent} from './components/Component.component';
-import {FunctionComponent} from './components/Function.component';
-import {CreateElementComponent} from './components/CreateElement.component';
-import {PureComponent} from './components/Pure.component';
+import { ComponentComponent } from './components/Component.component';
+import { FunctionComponent } from './components/Function.component';
+import { CreateElementComponent } from './components/CreateElement.component';
+import { PureComponent } from './components/Pure.component';
 
 export class App extends React.PureComponent {
     public render() {
         return (
             <main>
-                <PureComponent/>
-                <ComponentComponent/>
-                <FunctionComponent/>
-                <CreateElementComponent/>
+                <PureComponent />
+                <ComponentComponent />
+                <FunctionComponent />
+                <CreateElementComponent />
             </main>
         );
     }
